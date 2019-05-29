1baef0d95c4f3d051d8d4521bb39a3979e22e950
psychozaur <m.rybicki1112@gmail.com>
2019-05-29 11:28:51 +0200

    [change] - random number generated each click! now to figure out how to store a global variable and why alerts and console.logs don't work...

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,12 +1,21 @@
+randomQuestionIndex = 0;
+console.log(randomQuestionIndex);
+
 function drawQuestion(question){
     document.querySelector(".question").textContent="" + question;
     console.log(42);
 }
 
 function showAnswer(count){
+    console.log(randomQuestionIndex);
+    console.log(count);
+    alert('JC! A bomb!');
     document.querySelector(".answer").textContent="Answer was..." + count + "?";
 }
 
 function generateRandomIndex(count){
-    document.querySelector(".question").textContent= "" + Math.floor(Math.random() * count);
+    randomQuestionIndex = Math.floor(Math.random() * count);
+    console.log(randomQuestionIndex);
+    alert('JC! A bomb!');
+    document.querySelector(".question").textContent= "" + randomQuestionIndex;
 }
