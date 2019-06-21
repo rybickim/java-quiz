4310f11d61cb8f445b399038e401e612399302c8
psychozaur <m.rybicki1112@gmail.com>
2019-06-21 15:39:23 +0200

    [change] - adapted from the Baeldung's example; test always passes? I see no logs...

--- src/test/java/com/rybickim/javaquiz/JavaQuizDatabaseTest.java
+++ src/test/java/com/rybickim/javaquiz/JavaQuizDatabaseTest.java
@@ -8,26 +8,38 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { JavaQuizApplication.class },
+                    loader = AnnotationConfigContextLoader.class)
+@Transactional
+@DirtiesContext
 public class JavaQuizDatabaseTest {
 
     private static final Logger logger = LoggerFactory.getLogger(JavaQuizDatabaseTest.class);
 
-    @Qualifier("service")
-    StartService startService;
-
-//    @Qualifier("quizRepo")
-//    QuizEntityRepository repository;
+    @Resource
+    @Qualifier("quizRepo")
+    QuizEntityRepository quizEntityRepository;
 
     @Test
     public void testIfDatabaseIsCreated(){
 
-        logger.debug("Quizzes found with findAll():");
-        logger.debug("-------------------------------");
-        startService.getQuizExercises().forEach(quiz -> logger.debug(quiz.toString()));
-        logger.debug("");
+        logger.debug("--------------------------");
+        logger.debug("testIfDatabaseIsCreated(): {}", quizEntityRepository);
+        logger.debug("--------------------------");
+        assertNotNull(quizEntityRepository);
 
     }
 
