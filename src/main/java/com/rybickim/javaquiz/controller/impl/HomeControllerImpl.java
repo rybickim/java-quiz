6df7e47fae5783ae0c416c31fb9f928b78be5e6c
psychozaur <m.rybicki1112@gmail.com>
2019-09-11 10:32:43 +0200

    [change] - maybe th:src should be corrected to properly display a deserialized image? perhaps, it never gets deserialized because it never knows it's a @ResponseBody? need more research

--- src/main/java/com/rybickim/javaquiz/controller/impl/HomeControllerImpl.java
+++ src/main/java/com/rybickim/javaquiz/controller/impl/HomeControllerImpl.java
@@ -3,9 +3,11 @@ package com.rybickim.javaquiz.controller.impl;
 import com.rybickim.javaquiz.controller.HomeController;
 import com.rybickim.javaquiz.domain.Questions;
 import com.rybickim.javaquiz.service.CategoryService;
+import com.rybickim.javaquiz.service.ExplanationService;
 import com.rybickim.javaquiz.service.QuestionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,10 +22,12 @@ public class HomeControllerImpl implements HomeController {
 
     private QuestionService questionService;
     private List<Questions> questionsToShow;
+    private ExplanationService explanationService;
 
-    public HomeControllerImpl(QuestionService questionService) {
+    public HomeControllerImpl(QuestionService questionService, ExplanationService explanationService) {
         this.questionService = questionService;
         this.questionsToShow = questionService.listQuestions();
+        this.explanationService = explanationService;
     }
 
     @PostMapping({"/","/home"})
@@ -39,7 +43,7 @@ public class HomeControllerImpl implements HomeController {
         return "home";
     }
 
-    @GetMapping({"/","/home"})
+    @GetMapping(value = {"/","/home"}, produces = MediaType.IMAGE_PNG_VALUE)
     @Override
     public String homePage(Model dataModel) {
         logger.debug("homePage()");
@@ -50,19 +54,24 @@ public class HomeControllerImpl implements HomeController {
 
         String question = "";
         String answer = "";
+        String base64image = "";
 
         if (!questionsToShow.isEmpty()){
             question = questionsToShow.get(0).getQuestion();
             //TODO answers
 //            answer = questionsToShow.get(0).getCategories().getCategoryName();
+            base64image = Base64.getEncoder().encodeToString(explanationService.listExplanations().get(0).getExplanationDiagram());
         }
         logger.debug("question: {}", question);
         logger.debug("answer: {}", answer);
+        //limited to substring to actually read the logs
+        logger.debug("base64image: {}", base64image.substring(0,100));
 
 
         dataModel.addAttribute("question", question);
         dataModel.addAttribute("answer", answer);
         dataModel.addAttribute("count", quizExerciseCount);
+        dataModel.addAttribute("image", base64image);
 
         return "home";
     }
