0b850be17b8862e7f14d306b50bd551b1f2dd361
psychozaur <m.rybicki1112@gmail.com>
2019-09-30 15:20:20 +0200

    [change] - my bad, there should have been string concatenation passed to the header; also, trying to use jquery and failing

--- src/main/java/com/rybickim/javaquiz/controller/impl/FileRestControllerImpl.java
+++ src/main/java/com/rybickim/javaquiz/controller/impl/FileRestControllerImpl.java
@@ -73,7 +73,7 @@ public class FileRestControllerImpl implements FileRestController {
 
         return ResponseEntity.ok()
                 .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"", resource.getFilename(), "\"")
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
 }
