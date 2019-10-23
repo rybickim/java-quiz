424ad8bf907db72e6f8f6fc4f949e0aa71d9d2b1
psychozaur <m.rybicki1112@gmail.com>
2019-10-23 14:41:28 +0200

    [change] - it turns out the ajax call is redundant when the html form executes POST as well (curiously, only one POST seems to be executed when both onsubmit="sendDTOToController()" and method="post" are present); I tweaked the href attr of script and css and the stylesheet loads properly now

--- src/main/resources/static/js/addQuiz.js
+++ src/main/resources/static/js/addQuiz.js
@@ -1,27 +1,27 @@
-function sendDTOToController(){
-
-    var question = $("#question").val();
-    var category = $("#category").val();
-
-    $.ajax({
-        type: "POST",
-        contentType: "application/json",
-        url: "/addQuiz/submit",
-        data: {
-            question: question,
-            category: category
-        },
-        success: function(data){
-            console.log("addQuestionToDb(): AJAX call was successful, data: " +
-            data.question + ", " + data.category);
-        },
-        error: function(e){
-            console.log("ERROR: ", e);
-        },
-        complete: function() {
-            console.log("addQuestionToDb(): AJAX call complete")
-        }
-    });
-
-    $(".addedNotification").textContent="Question was added";
-}
+// function sendDTOToController(){
+//
+//     var question = $("#question").val();
+//     var category = $("#category").val();
+//
+//     $.ajax({
+//         type: "POST",
+//         contentType: "application/json",
+//         url: "/addQuiz/submit",
+//         data: {
+//             question: question,
+//             category: category
+//         },
+//         success: function(data){
+//             console.log("addQuestionToDb(): AJAX call was successful, data: " +
+//             data.question + ", " + data.category);
+//         },
+//         error: function(e){
+//             console.log("ERROR: ", e);
+//         },
+//         complete: function() {
+//             console.log("addQuestionToDb(): AJAX call complete")
+//         }
+//     });
+//
+//     $(".addedNotification").textContent="Question was added";
+// }
